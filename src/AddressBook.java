494f69a146f4ea26b36f45ded59246f7fb0250f4
Tayo <temitayoadegoke@cmail.carleton.ca>
2020-10-02 11:45:17 -0400

    i commited file

---
+++ src/AddressBook.java
@@ -0,0 +1,43 @@
+
+    import java.util.ArrayList;
+    public class AddressBook {
+
+        private ArrayList<BuddyInfo> myBuddies;
+        public AddressBook() {
+            myBuddies = new ArrayList<>();
+        }
+
+
+        public void addBuddy(BuddyInfo aBuddy) {
+            if(aBuddy != null){
+                myBuddies.add(aBuddy);
+
+            }
+
+        }
+        public BuddyInfo removeBuddy(int index) {
+            if(index >=0 && index< myBuddies.size()){
+                return myBuddies.remove(index);
+            }
+            return null;
+        }
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
+            System.out.println("Address Book");
+            BuddyInfo buddy = new BuddyInfo ("Tom",  "Carleton",  "613");
+            AddressBook addressBook = new AddressBook();
+            addressBook.addBuddy(buddy);
+            addressBook.removeBuddy(0);
+
+
+        }
+
+    }
+
